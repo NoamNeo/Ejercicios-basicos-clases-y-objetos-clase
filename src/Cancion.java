1fe3e28f45767d24e5dbf95547d6a3b386acb3a3
NoamNeo <gabriel.loira.lorenzo@gmail.com>
2026-01-13 09:32:48 +0100

    first commit

---
+++ src/Cancion.java
@@ -0,0 +1,29 @@
+public class Cancion {
+  private String titulo;
+  private String artista;
+  private int duracion;
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getTitulo() {
+    return this.titulo;
+  }
+
+  public void setArtista(String artista) {
+    this.artista = artista;
+  }
+
+  public String getArtista() {
+    return this.artista;
+  }
+
+  public void setDuracion(int duracion) {
+    this.duracion = duracion;
+  }
+
+  public int getDuracion() {
+    return this.duracion;
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    // Ejercicio 1 canción:
+    Cancion cancion1 = new Cancion();
+    Cancion cancion2 = new Cancion();
+  }
+}
